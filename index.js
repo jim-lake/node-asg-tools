d63c7215debe9d8efb6234f7d9c8efd0fd652e6c
Jim Lake <jim@blueskylabs.com>
2019-11-18 15:17:13 -0800

    map not pluck

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ function fetchInstances(done) {
   (done) => {
     const ec2 = new AWS.EC2(g_aws_config);
 
-    const all_id_list = _.pluck(auto_scale_group.Instances,'InstanceId');
+    const all_id_list = auto_scale_group.Instances.map(i => i.InstanceId);
     const id_list = all_id_list.filter(i => i !== g_instance_id);
     if (id_list.length == 0) {
       done(null);
